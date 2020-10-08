18c945eb131603473d2dd3885b9aa01922acd716
Oscar Almgren <oscar.almgren@gmail.com>
2020-10-08 20:48:04 +0200

    ninja lvl 3

--- ninja2/ex2/main.go
+++ ninja2/ex2/main.go
@@ -2,8 +2,22 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 func main() {
-	fmt.Println("Hello")
+	rand.Seed(time.Now().UnixNano())
+	i := rand.Intn(6)
+	fmt.Println(i)
+	switch i {
+	case 1, 3, 5:
+		fmt.Println("This was odd")
+		//fallthrough
+	case 2, 4, 6:
+		fmt.Println("This was even")
+	default:
+		fmt.Println("Default")
+	}
+
 }
---
+++ ninja2/ex3/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	for i := 0; i < 10001; i++ {
+		if i%1000 == 0 {
+			fmt.Println(i)
+		}
+	}
+}
