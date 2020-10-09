16c8fb9a1e113cd51f0c1bfa6ff83e56f80af612
Oscar Almgren <oscar.almgren@gmail.com>
2020-10-09 10:27:59 +0200

    slices

--- ninja2/ex3/main.go
+++ ninja2/ex3/main.go
@@ -3,9 +3,26 @@ package main
 import "fmt"
 
 func main() {
-	for i := 0; i <= 1000; i++ {
-		if i%1000 == 0 {
-			fmt.Println(i)
-		}
+	// array
+	var x [5]int
+	x[1] = 42
+	fmt.Println(x)
+
+	// slice (allows you to GROUP together VALUES of the same TYPE)
+	xx := []int{11, 22, 33, 44, 55} // slice []type{} <- slice format
+	fmt.Println(xx)
+	fmt.Println(len(xx))
+	//fmt.Println(cap(xx))
+	fmt.Println("index 2:", xx[2])
+	for i, v := range xx {
+		fmt.Println(i, v)
+	}
+
+	// : operator for slicing slices
+	fmt.Println(xx[1:])  // start at this position
+	fmt.Println(xx[1:3]) // up to but not including 3rd position
+
+	for i := 0; i <= 4; i++ {
+		fmt.Println(i, xx[i])
 	}
 }
