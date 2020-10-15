5ca2fd2b4e279d9435945f834238aa8588e7a7b4
Oscar Almgren <oscar.almgren@gmail.com>
2020-10-15 11:30:35 +0200

    comments on exp or imp fields

--- ninja2/ex5/structs/main.go
+++ ninja2/ex5/structs/main.go
@@ -2,20 +2,42 @@ package main
 
 import "fmt"
 
+// create a new type (struct). complex data type
 type person struct {
-	first string
+	// Field names may be...
+
+	first string // explicitly (IdentifierList)
 	last  string
+	age   int
+}
+
+// combine structs to new types
+type secretAgent struct {
+	// Field names may be...
+	person //  implicitly (EmbeddedField)
+	ltk    bool
 }
 
 // structs
 func main() {
+	// create a _value_ of type "person" (object)
 	p1 := person{
 		first: "Oscar",
 		last:  "Almgren",
+		age:   38,
 	}
 	p2 := person{
 		first: "Ida",
 		last:  "Almgren",
+		age:   29,
 	}
-	fmt.Println(p1, "\n", p2)
+	sa := secretAgent{
+		person: person{
+			first: "James",
+			last:  "Bond",
+			age:   47,
+		},
+		ltk: true,
+	}
+	fmt.Println(p1, "\n", p2, "\n", sa)
 }
