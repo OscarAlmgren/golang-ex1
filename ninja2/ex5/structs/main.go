fbfd71e2bea56fcfef0098b5c8fa3853c78a6122
Oscar Almgren <oscar.almgren@gmail.com>
2020-10-15 10:44:30 +0200

    structs are like objects/class (almost)

--- ninja2/ex5/structs/main.go
+++ ninja2/ex5/structs/main.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
+type person struct {
+	first string
+	last  string
+}
+
+// structs
 func main() {
-	fmt.Println("Hello")
+	p1 := person{
+		first: "Oscar",
+		last:  "Almgren",
+	}
+	p2 := person{
+		first: "Ida",
+		last:  "Almgren",
+	}
+	fmt.Println(p1, "\n", p2)
 }
