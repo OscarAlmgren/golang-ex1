6a06b040501b9748534941e9bffda554cc25f3d8
Oscar Almgren <oscar.almgren@gmail.com>
2020-10-21 15:56:27 +0200

    multi return funcs

--- ninja2/ex6/functions/main.go
+++ ninja2/ex6/functions/main.go
@@ -5,6 +5,10 @@ import "fmt"
 func main() {
 	foo()
 	bar("Oscar")
+	s1 := woo("Fredrik")
+	fmt.Println(s1)
+	x, y := mouse("Ian", "Fleming")
+	fmt.Println(x, y)
 }
 
 // func (r receiver) identifier(parameter) (return(s)) { code ...}
@@ -16,4 +20,14 @@ func foo() {
 // everything in Go is PASS BY VALUE
 func bar(s string) {
 	fmt.Println("hello,", s)
+}
+
+func woo(s string) string {
+	return fmt.Sprint("Hello from woo,", s)
+}
+
+func mouse(fn, ln string) (string, bool) {
+	a := fmt.Sprint(fn, ln, ` , says "Hello"`)
+	b := false
+	return a, b
 }
