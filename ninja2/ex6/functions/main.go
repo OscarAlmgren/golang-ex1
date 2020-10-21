cac60b4b7dc6707517d4be204062305a145aae44
Oscar Almgren <oscar.almgren@gmail.com>
2020-10-21 15:50:45 +0200

    functions initial.

--- ninja2/ex6/functions/main.go
+++ ninja2/ex6/functions/main.go
@@ -3,5 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello")
+	foo()
+	bar("Oscar")
+}
+
+// func (r receiver) identifier(parameter) (return(s)) { code ...}
+
+func foo() {
+	fmt.Println("hello from foo")
+}
+
+// everything in Go is PASS BY VALUE
+func bar(s string) {
+	fmt.Println("hello,", s)
 }
