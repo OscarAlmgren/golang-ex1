a28c57e57646d61cc38b8936e761fe35969f213c
Oscar Almgren <oscar.almgren@gmail.com>
2020-11-14 12:42:08 +0100

    json std out

--- ninja2/ex8/ex/main.go
+++ ninja2/ex8/ex/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"os"
 )
 
 type user struct {
@@ -60,4 +61,13 @@ func main() {
 			fmt.Println("Sayings:", saying)
 		}
 	}
+
+	// Write to std out
+	json.NewEncoder(os.Stdout).Encode(persons)
+
+	xi := []int{5, 8, 2, 43, 17, 987, 14, 12, 21, 1, 4, 2, 3, 93, 13}
+	fmt.Println(xi)
+
+	xs := []string{"random", "rainbow", "delights", "in", "torpedo", "summers", "under", "gallantry", "fragmented", "moons", "across", "magenta"}
+	fmt.Println(xs)
 }
