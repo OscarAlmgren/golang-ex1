52c3d1d45d83e506cefe3b89e8e11df8dc0166c3
Oscar Almgren <oscar.almgren@gmail.com>
2020-11-17 11:28:23 +0100

    exercise 9

--- ninja2/ex9/ex/main.go
+++ ninja2/ex9/ex/main.go
@@ -1,43 +1,56 @@
+/* Ninja level 9 */
+
 package main
 
 import (
 	"fmt"
-	"runtime"
-	"sync"
-	"sync/atomic"
 )
 
+type person struct {
+	first string
+	last  string
+	age   int
+}
+
+type human interface {
+	speak()
+}
+
+func (p *person) speak() {
+	fmt.Println("Spoken from speak for", p.first)
+}
+
+func saySomething(h human) {
+	h.speak()
+}
+
 func main() {
-	fmt.Println("CPUs:", runtime.NumCPU())
-	fmt.Println("Go runs:", runtime.NumGoroutine())
-	fmt.Printf("GOMAXPROCS:%d\n", runtime.GOMAXPROCS(0))
 
-	counter := 0
-	var atomicCounter int64
-	const gs = 100
-
-	var wg sync.WaitGroup
-	wg.Add(gs)
-
-	var mu sync.Mutex
-
-	for i := 0; i < gs; i++ {
-		go func() {
-			atomic.AddInt64(&atomicCounter, 1)
-			fmt.Println("Atomic:", atomic.LoadInt64(&atomicCounter))
-			mu.Lock()
-			v := counter
-			// time.Sleep(time.Second)
-			runtime.Gosched() // hey start another thread
-			v++
-			counter = v
-			mu.Unlock()
-			wg.Done()
-		}()
-		fmt.Println("Go runs:", runtime.NumGoroutine())
+	p1 := person{
+		first: "Oscar",
+		last:  "Almgren",
+		age:   38,
 	}
 
-	wg.Wait()
-	fmt.Println("Go runs:", runtime.NumGoroutine())
-	fmt.Println("Count:", counter)
+	saySomething(&p1)
+	// var wg sync.WaitGroup
+	// wg.Add(2)
+
+	// go func() {
+	// 	fmt.Println("hello from 1")
+	// 	wg.Done()
+	// }()
+
+	// go func() {
+	// 	fmt.Println("hello from 2")
+	// 	wg.Done()
+	// }()
+
+	// fmt.Println("Mid go runs:", runtime.NumGoroutine())
+
+	// wg.Wait()
+	// fmt.Println("CPU:", runtime.NumCPU())
+	// fmt.Println("GOMAX:", runtime.GOMAXPROCS(0))
+	// fmt.Println("Go runs:", runtime.NumGoroutine())
+	// fmt.Println("About to exit")
 }
