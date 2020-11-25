fe28117a3e30242a92a867a31b493ddbbeafc940
Oscar Almgren <oscar.almgren@gmail.com>
2020-11-25 22:38:07 +0100

    lol

--- ninja2/ex10/channels/main.go
+++ ninja2/ex10/channels/main.go
@@ -8,7 +8,7 @@ func main() {
 		c <- 42
 	}()
 
-	d := make(chan int, 1) // buffered channel
+	d := make(chan int, 1) // buffered channel (try to stay away from them according to tips)
 	d <- 131
 
 	fmt.Println(<-c)
