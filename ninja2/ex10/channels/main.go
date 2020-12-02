dad5eb047e3a7772bb96660590d14ad69c713702
Oscar Almgren <oscar.almgren@gmail.com>
2020-12-02 15:25:21 +0100

    send and receive channel

--- ninja2/ex10/channels/main.go
+++ ninja2/ex10/channels/main.go
@@ -3,14 +3,33 @@ package main
 import "fmt"
 
 func main() {
-	c := make(chan int) // non-buffered channel
-	go func() {
-		c <- 42
-	}()
+	// c := make(chan int) // non-buffered channel
+	// go func() {
+	// 	c <- 42
+	// }()
 
-	d := make(chan int, 1) // buffered channel (try to stay away from them according to tips)
-	d <- 131
+	// d := make(chan int, 1) // buffered channel (try to stay away from them according to tips)
+	// d <- 131
 
-	fmt.Println(<-c)
-	fmt.Println(<-d)
+	// fmt.Println(<-c)
+	// fmt.Println(<-d)
+
+	c := make(chan int)
+
+	go foo(c)
+
+	bar(c)
+
+	fmt.Println("Exit")
+
+}
+
+// send
+func foo(c chan<- int) {
+	c <- 42
+}
+
+// receive
+func bar(c <-chan int) {
+	fmt.Println("Value:", <-c)
 }
